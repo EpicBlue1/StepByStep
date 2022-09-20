0a3ad721157725d18bffe9ac6475228d341e97fb
EpicBlue1 <90909758+EpicBlue1@users.noreply.github.com>
2022-09-20 20:41:22 +0200

    Project Set Up

---
+++ StepByStep/StepByStep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StepByStep
+//
+//  Created by Reinhardt de Eier on 29/06/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StepByStep/StepByStep/StepByStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepByStepApp.swift
+//  StepByStep
+//
+//  Created by Reinhardt de Eier on 29/06/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct StepByStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
