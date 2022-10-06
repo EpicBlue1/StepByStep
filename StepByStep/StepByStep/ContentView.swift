207aab4c5a3b23fb72e894673aff67e3182f5b9e
EpicBlue1 <90909758+EpicBlue1@users.noreply.github.com>
2022-10-06 12:52:49 -0700

    Basic layout done
    GITHUB WORKS

--- StepByStep/StepByStep/ContentView.swift
+++ StepByStep/StepByStep/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Rectangle()
+                    .frame(width: .infinity, height: 100, alignment: .trailing)
+                    .cornerRadius(25)
+                    .overlay(
+                        HStack{
+                            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                                .frame(width: 225, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            Image(systemName: "gearshape.fill")
+                                .foregroundColor(.white)
+                                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }
+                )
+            }
+            HStack{
+                Text("Projects")
+                Text("18")
+                Spacer()
+                
+            }.padding()
+            ScrollView {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+            }.frame(width: .infinity, height: .infinity, alignment: .center)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
